9c24b8f06bf929d7e22978cb73f65378b1b71029
Ishant Sharma <ishantsh12234@gmail.com>
2023-10-01 14:33:34 +0530

    New PDF format and some fixes

--- export.js
+++ export.js
@@ -1,73 +1,72 @@
-function exportAsPDF(){
+
+function exportAsPDF() {
     const doc = new window.jspdf.jsPDF({
         unit: "pt",
         orientation: "p",
         lineHeight: 1.3
     });
-    doc.addFont("fonts/Roboto-Regular.ttf", "Roboto", "normal");
-    doc.addFont("fonts/Roboto-Bold.ttf", "Roboto", "bold");
-    doc.addFont("fonts/Candara.ttf", "Candara", "normal");
-    doc.addFont("fonts/Candara_Bold.ttf", "Candara", "bold");
-    
-    
-    doc.setFont("Roboto");
-    doc.setFont(undefined, "bold");
+
     doc.setFontSize(28);
+    doc.setFont(undefined, 'bold');
     doc.text("DHL Duty", 20, 50);
     doc.line(10, 58, 585, 58);
-    
-    var out = `
-    CI Value(USD/EUR): 
-    Exchange Rate:: 
-    Freight: 
-    BCD(%): 
+    doc.setFont(undefined, 'normal');
 
-    INR value: 
-    Insurance:
-    Total:
+    doc.setFontSize(14);
+    doc.autoTable({
+        startY: 100,
+        body: getRows(),
+        styles: {
+            lineColor: [0, 0, 0],
+            lineWidth: 0.2,
+        },
+        bodyStyles: {
+            fillColor: [255, 255, 255],
+            textColor: 0,
+        },
+        alternateRowStyles: {
+            fillColor: [255, 255, 255],
+        },
+        columnStyles: {
+            0: {
+                fontStyle: 'bold',
+            }
+        },
 
-    BCD: 
-    SW:
-    GST:
+        didDrawPage: function (data) {
+        }
+    });
 
-    Duty Payable Amount:
-    `
-    
-    doc.setFont("Candara", "bold");
-    doc.setFontSize(18);
-    doc.text(out, 20, 80);
 
-    out = `
-    ${ci}
-    ${r}
-    ${fr}
-    ${BCD_rate}%
-
-    ${inr}
-    ${insurance}
-    ${total}
-
-    ${BCD}
-    ${SW}
-    ${gstAmt}
-
-    ${amt}
-    `;
-    doc.setFont("Candara", "normal");
-    doc.setFontSize(18);
-    doc.text(out, 260, 80);
-
-    out = `Rupees ${getWord(amt)} only.`
-    doc.text(out, 20, 450)
+    doc.setFontSize(12);
+    doc.text(`Rupees ${getWord(amt)} only.`, 20, 450)
 
     let currentDateAndTime = new Date();
-    out = `${currentDateAndTime.getDate()}-${currentDateAndTime.getMonth()}-${currentDateAndTime.getFullYear()} | ${currentDateAndTime.getHours()}:${currentDateAndTime.getMinutes()}:${currentDateAndTime.getSeconds()}`;
-    doc.setFont("Roboto", "normal");
-    doc.setFontSize(14);
-    doc.line(10, 490, 585, 490);
-    doc.text(out, 20, 507);
-    doc.save("export.pdf");
+    let out = `${currentDateAndTime.getDate()}-${currentDateAndTime.getMonth()}-${currentDateAndTime.getFullYear()} | ${currentDateAndTime.getHours()}:${currentDateAndTime.getMinutes()}:${currentDateAndTime.getSeconds()}`;
+
+    doc.line(10, 470, 585, 470);
+    doc.text(out, 20, 481);
+
+    doc.save();
 }
+
+function getRows() {
+    let rows = []
+
+    rows.push([`CI Value ${curr.innerText}`, ci]);
+    rows.push(["Exchange Rate", r]);
+    rows.push(["Freight", fr]);
+    rows.push(["BCD(%)", `${BCD_rate}%`]);
+    rows.push(["INR Value", inr]);
+    rows.push(["Insurance", insurance]);
+    rows.push(["Total", total]);
+    rows.push(["BCD", BCD]);
+    rows.push(["SW", SW]);
+    rows.push(["GST", gstAmt]);
+    rows.push(["Duty Payable Amount", amt]);
+    return rows;
+}
+
 
 const exportBtn = document.getElementById("genPDFbtn");
 exportBtn.addEventListener('click', exportAsPDF);
