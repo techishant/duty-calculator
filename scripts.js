a1da82b115ec75efc9f402debff4672c6081c2d6
Ishant Sharma <ishantsh12234@gmail.com>
2023-12-13 21:59:13 +0530

    List made functional

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const rateValue = document.getElementById('rateValue');
 const freightValue = document.getElementById('freightValue');
 const select_BCD_rate = document.getElementById("BCDrate");
 const GSTval = document.getElementsByName('GSToption');
-const DutyName = document.getElementById('dutyName');
+const dutyNameSelector = document.getElementById('dutyNameSelector');
 
 //OUTPUT
 const INRvalue = document.getElementById('INRvalue');
@@ -20,34 +20,44 @@ const gstAmtValue = document.getElementById("gstAmtVal");
 // EUR Or USD
 const currLabel = document.getElementById('curr');
 
-const duty1 = new Duty();
+const newDutyBtn = document.getElementById("newDutyBtn");
 
-let ci, r, fr, BCD_rate;
-let inr = 0;
-let FR = 0;
-let insurance = 0;
-let total = 0;
+// LISTS
+const BillsListPanel = document.getElementById("BillsList");
 
-let BCD = 0;
-let SW = 0;
-let amt = 0;
-let gst = 0;
-let gstAmt = 0;
-
+let billsList = Array(1).fill(new Duty());
+let currentDuty = 0;
 
 var BCD_Rates_List = [10, 15, 20, 25];
+const duty_name_list = ["HARDWARE", "NON-HARDWARE", "OTHERS"];
+
+newDutyBtn.addEventListener('click', () => {
+    billsList.push(new Duty());
+    currentDuty++;
+    resetForm();
+    calculateValues();
+});
+
 
 CIvalue.addEventListener('input', updateCI_rate);
 rateValue.addEventListener('input', updateCI_rate);
 freightValue.addEventListener('input', updateFreight);
-DutyName.addEventListener('input', calculateValues);
 for (i = 0; i < GSTval.length; i++) {
     GSTval[i].addEventListener('input', calculateValues);
 }
 select_BCD_rate.addEventListener('change', calculateValues);
+dutyNameSelector.addEventListener('change', calculateValues);
+
+let opt;
+for (i = 0; i < duty_name_list.length; i++) {
+    opt = document.createElement("option")
+    opt.value = duty_name_list[i];
+    opt.innerText = `${duty_name_list[i]}   `;
+    dutyNameSelector.appendChild(opt);
+}
 
 // Adding those options to DOM throgh JS
-let opt;
+opt;
 for (i = 0; i < BCD_Rates_List.length; i++) {
     opt = document.createElement("option")
     opt.value = BCD_Rates_List[i];
@@ -77,60 +87,121 @@ function updateCI_rate() {
 
 calculateValues();
 
-function calculateValues(){
+function calculateValues() {
+    billsList[currentDuty].curr = curr.innerText;
 
-    duty1.curr = curr.innerText;
-
-    duty1.name = DutyName.value;
-    duty1.inputCI$R(CIvalue.value, rateValue.value);
+    billsList[currentDuty].name = dutyNameSelector.value;
+    billsList[currentDuty].inputCI$R(parseFloat(CIvalue.value), parseFloat(rateValue.value));
     /**
      * this.inr = this.ci * this.r
      */
-    duty1.calculateINRvalue();
+    billsList[currentDuty].calculateINRvalue();
 
 
     if (isupdateCI_rate) {
-        duty1.calculateFrValue();
-        freightValue.value =  duty1.getFr();
+        billsList[currentDuty].calculateFrValue();
+        freightValue.value = billsList[currentDuty].getFr();
         isupdateCI_rate = false;
     }
-    
+
     if (isFRupdated == false) {
-        duty1.calculateFrValue();
-        freightValue.value = duty1.getFr();
+        billsList[currentDuty].calculateFrValue();
+        freightValue.value = billsList[currentDuty].getFr();
         isFRupdated = true;
     }
 
-    duty1.calculateFrValue(parseFloat(freightValue.value));
+    billsList[currentDuty].calculateFrValue(parseFloat(freightValue.value));
 
-    duty1.calculateInsurance();
-    duty1.bcd_rate = parseFloat(select_BCD_rate.value);
+    billsList[currentDuty].calculateInsurance();
+    billsList[currentDuty].bcd_rate = parseFloat(select_BCD_rate.value);
 
-    duty1.calculateTotal();
+    billsList[currentDuty].calculateTotal();
 
-    duty1.calculateBCDandSW();
+    billsList[currentDuty].calculateBCDandSW();
 
     for (i = 0; i < GSTval.length; i++) {
         if (GSTval[i].checked) {
-            duty1.gst = parseFloat(GSTval[i].value);
+            billsList[currentDuty].gst = parseFloat(GSTval[i].value);
             break;
         }
     }
 
-    duty1.calculateGstAndAmount();
+    billsList[currentDuty].calculateGstAndAmount();
 
+    flipOutputs();
+    updateList()
+}
+
+
+function flipOutputs() {
+    INRvalue.innerText = `₹${billsList[currentDuty].inr}`;
+    freightFinalValue.innerText = `₹${billsList[currentDuty].fr}`;
+    insuranceValue.innerText = `₹${billsList[currentDuty].insurance}`;
+    totalValue.innerText = `₹${billsList[currentDuty].total}`;
+    BCDvalue.innerText = `₹${billsList[currentDuty].bcd}`;
+    SWvalue.innerText = `₹${billsList[currentDuty].sw}`;
+    amtValue.innerText = `₹${billsList[currentDuty].amt}`;
+    gstAmtValue.innerText = `₹${billsList[currentDuty].gstAmt}`;
+}
+
+function flipForm() {
+    dutyNameSelector.value = billsList[currentDuty].name;
+    CIvalue.value = billsList[currentDuty].ci;
+    curr.innerText = billsList[currentDuty].curr;
+    rateValue.value = billsList[currentDuty].r;
+    freightValue.value = billsList[currentDuty].fr;
+    BCDvalue.value = billsList[currentDuty].bcd;
+    if (billsList[currentDuty].gst == 12) {
+        GSTval[0].checked = true;
+    } else {
+        GSTval[0].checked = false;
+    }
+
+}
+
+function resetForm() {
+    curr.innerText = "(USD)";
+    dutyNameSelector.value = "HARDWARE";
+    CIvalue.value = 100;
+    rateValue.value = 85;
+    BCDvalue.value = "10%";
+    GSTval[1].checked = true;
+    calculateValues();
+    freightValue.value = billsList[currentDuty].getFr()
+    flipOutputs();
+}
+
+function changeCurrentDuty(newDutyIndex) {
+    currentDuty = newDutyIndex;
+    updateList();
     flipForm();
+    flipOutputs();
+    // calculateValues();
 }
 
 
-function flipForm(){
-    INRvalue.innerText = `₹${duty1.inr}`;
-    freightFinalValue.innerText = `₹${duty1.fr}`;
-    insuranceValue.innerText = `₹${duty1.insurance}`;
-    totalValue.innerText = `₹${duty1.total}`;
-    BCDvalue.innerText = `₹${duty1.bcd}`;
-    SWvalue.innerText = `₹${duty1.sw}`;
-    amtValue.innerText = `₹${duty1.amt}`;
-    gstAmtValue.innerText = `₹${duty1.gstAmt}`;
-}
+function updateList() {
+    console.log(billsList);
+    let item;
+    BillsListPanel.innerHTML = "";
+    for (let j = 0; j < billsList.length; j++) {
+        item = document.createElement("li");
+        item.classList.remove("selected");
+        item.classList = (currentDuty === j) ? "selected" : "";
+        item.innerHTML = `<span class="name">${billsList[j].name}</span>
+        <span>SW: ${billsList[j].sw}</span>
+        <span>GST: ${billsList[j].gstAmt}</span>
+        <span>Duty Payable Amount: ${billsList[j].amt}</span>
 
+        <span class="action-panel">
+            <button>Delete</button>
+            <button>Edit</button>
+        </span>`;
+
+        item.addEventListener("click", () => {
+            console.log(j)
+            changeCurrentDuty(j);
+        });
+        BillsListPanel.append(item);
+    }
+}
